97bc6bec8335144ddc5842968e4865ef7c759950
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-23 12:04:52 +0200

    added validation on resumes & portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,7 @@
 class Portfolio < ApplicationRecord
   belongs_to :resume
+
+  validates :title, presence: true
+  validates :url, presence: true, uniqueness: true
+  validates :description, presence: true
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,7 @@
 class Resume < ApplicationRecord
   belongs_to :user
+  has_many :portfolios
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
