0cfb99edc6d22189afee5de5ee62db72070dbd19
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-29 16:13:21 +0100

    added create action for resumes

--- app/controllers/api/v1/resumes_controller.rb
+++ app/controllers/api/v1/resumes_controller.rb
@@ -1,5 +1,29 @@
 class Api::V1::ResumesController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index ]
+
   def index
     @resumes = policy_scope(Resume)
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    @resume.user = current_user
+    authorize @resume
+    if @resume.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:title, :description)
+  end
+
+  def render_error
+    render json: { errors: @resume.errors.full_messages },
+    status: :unprocessable_entity
   end
 end
--- app/policies/resume_policy.rb
+++ app/policies/resume_policy.rb
@@ -4,4 +4,8 @@ class ResumePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
 end
