481d3f15fb47253133f1d020b84c047aaba70581
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-20 16:39:53 +0100

    added test for #create on resumes controller

--- spec/controllers/api/v1/resumes_controller_spec.rb
+++ spec/controllers/api/v1/resumes_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ResumesController, type: :controller do
+  before do
+    emma = User.create(first_name: 'Emma', last_name: 'Smith', email: 'emma@test.com', password: '123456')
+  end
 
+  describe "#create" do
+    it "creates a new resume" do
+      user = User.first
+      resumes = Resume.count
+      post :create, params: {
+        "resume": {
+          "title":"Architect",
+          "description":"Bauhaus is my life <3"
+         },
+         "user_email": user.email,
+         "user_token": user.authentication_token
+        }, xhr: true
+      expect(Resume.count).to eq(resumes + 1)
+    end
+  end
 end
