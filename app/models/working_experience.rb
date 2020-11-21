d7a64bd2a430c110803470f5bd8277123a32ee3f
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-21 13:36:56 +0100

    added validations for WorkingExperience

--- app/models/working_experience.rb
+++ app/models/working_experience.rb
@@ -1,3 +1,7 @@
 class WorkingExperience < ApplicationRecord
   belongs_to :resume
+
+  validates :start_date, presence: true
+  validates :title, presence: true
+  validates :company, presence: true
 end
