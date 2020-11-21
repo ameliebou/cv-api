c79e7bb6192598f8629d2cbec5ba636bdf56dbf4
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-21 11:59:28 +0100

    added validations to Education model

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,7 @@
 class Education < ApplicationRecord
   belongs_to :resume
+
+  validates :graduation_date, presence: true
+  validates :degree, presence: true
+  validates :institute, presence: true
 end
